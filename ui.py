5d59beb9e63a94a7b894c1474fbe080777cf4f47
Dennis Schmalstieg <darianbellentor@outlook.de>
2023-01-01 20:00:31 +0100

    first graphical button

--- ui.py
+++ ui.py
@@ -15,7 +15,10 @@ class QuizInterface:
         self.txt_question = self.canvas.create_text(150, 125, text="The question", fill=THEME_COLOR, font=("Arial", 18, "normal"))
         self.canvas.grid(row=1, column=0, columnspan=2, pady=20)
 
-        
+        btn_img_true = PhotoImage(file="images/true.png")
+        btn_img_false = PhotoImage(file="images/true.png")
+        self.btn_true = Button(image=btn_img_true)
+        self.btn_true.grid(row=2, column=0)
 
 
         self.window.mainloop()
