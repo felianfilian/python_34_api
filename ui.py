3038d827b574d1f83a3b22411f4af0006eb74476
Dennis Schmalstieg <darianbellentor@outlook.de>
2023-01-01 19:58:32 +0100

    question canvas

--- ui.py
+++ ui.py
@@ -8,9 +8,14 @@ class QuizInterface:
         self.window.title("Trivial Quiz")
 
         self.window.config(width=400, height=600, padx=20, pady=20, bg=THEME_COLOR)
-        txt_score = Label(text="Score: 0", fg="white", font=("Arial", 18, "normal"), bg=THEME_COLOR, highlightthickness=0,  padx=20, pady=20)
-        txt_score.grid(row=0, column=1)
-        txt_quest = Label(text="the question", fg="black", font=("Arial", 18, "normal"), bg="white", padx=20, pady=20)
-        txt_quest.grid(row=1, column=0, columnspan=2)
+        self.txt_score = Label(text="Score: 0", fg="white", font=("Arial", 18, "normal"), bg=THEME_COLOR, highlightthickness=0,  padx=20, pady=20)
+        self.txt_score.grid(row=0, column=1)
+
+        self.canvas = Canvas(width=300, height=250, bg="white")
+        self.txt_question = self.canvas.create_text(150, 125, text="The question", fill=THEME_COLOR, font=("Arial", 18, "normal"))
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=20)
+
+        
+
 
         self.window.mainloop()
