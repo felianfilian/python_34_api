7da69a44b1900a488d37ba2d1f77cfd9a69c5b15
Dennis Schmalstieg <darianbellentor@outlook.de>
2023-01-01 19:39:14 +0100

    first layout elements

--- ui.py
+++ ui.py
@@ -7,5 +7,10 @@ class QuizInterface:
         self.window = Tk()
         self.window.title("Trivial Quiz")
 
+        self.window.config(width=400, height=600, padx=20, pady=20, bg=THEME_COLOR)
+        txt_score = Label(text="Score: 0", fg="white", font=("Arial", 18, "normal"), bg=THEME_COLOR, highlightthickness=0,  padx=20, pady=20)
+        txt_score.grid(row=0, column=1)
+        txt_quest = Label(text="the question", fg="black", font=("Arial", 18, "normal"), bg="white", padx=20, pady=20)
+        txt_quest.grid(row=1, column=0, columnspan=2)
 
         self.window.mainloop()
